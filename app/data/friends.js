986649b9e8597139dd8406dd49382d4f8def3d46
ralracish <carlarblackman@gmail.com>
2019-03-17 16:38:27 -0400

    working files with photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,122 @@
 var friends = [
+
   {
-    "name": "Benjamin Franklin",
-    "photo": "https://en.wikipedia.org/wiki/Benjamin_Franklin#/media/File:Benjamin_Franklin_by_Joseph_Duplessis_1778.jpg",
-    "scores": [3, 4, 2, 1, 5, 4, 6, 4, 2, 2]
-  },
-  {
-    "name":"Laura Bush",
-    "photo": "https://en.wikipedia.org/wiki/Laura_Bush#/media/File:Laura_Bush_portrait.jpg/",
-    "scores": [1, 1, 2, 3, 1, 3, 5, 6, 7, 3]
-  },
-  {
-    "name":"Hillary Clinton",
-    "photo": "https://en.wikipedia.org/wiki/Hillary_Clinton#/media/File:Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg",
+    "name": "Barbara Johnson",
+    "photo": "https://images.unsplash.com/photo-1465405319612-e87fd78c23f6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
     "scores": [4, 5, 3, 3, 4, 2, 4, 5, 1, 1]
   },
   {
-    "name":"Jimmy Carter",
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/5/5a/JimmyCarterPortrait2.jpg",
+    "name": "Janine Jones",
+    "photo": "https://images.unsplash.com/photo-1517323882654-d0a13cbc9668?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [3, 4, 2, 1, 5, 4, 6, 4, 2, 2]
+  },
+  {
+    "name": "Susan Ward",
+    "photo": "https://images.unsplash.com/photo-1436985847904-40f78a6bef9d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 1, 2, 3, 1, 3, 5, 6, 7, 3]
+  },
+  { 
+    "name": "Abby Vaughn",
+    "photo": "https://images.unsplash.com/photo-1516795680264-ba1f8518c603?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores":  [4, 5, 3, 3, 4, 2, 4, 5, 1, 1]
+  },
+  { "name": "John Hetherton",
+    "photo": "https://images.unsplash.com/photo-1497184091687-09d30ae055e1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
     "scores": [5, 1, 3, 2, 1, 4, 5, 1, 4, 3]
   },
   {
-    "name":"John F. Kennedy",
-    "photo": "https://en.wikipedia.org/wiki/John_F._Kennedy#/media/File:John_F._Kennedy,_White_House_photo_portrait,_looking_up.jpg",
-    "scores": [1, 2, 2, 4, 1, 3, 5, 2, 1, 3]
+    "name": "Andy Clark",
+    "photo": "https://images.unsplash.com/photo-1478136969008-4d5857e9dc8c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [5, 1, 3, 2, 1, 4, 5, 1, 4, 3]
   },
   {
-    "name":"Eleanor Roosevelt",
-    "photo": "https://en.wikipedia.org/wiki/Eleanor_Roosevelt#/media/File:Eleanor_Roosevelt_portrait_1933.jpg",
-    "scores": [2, 4, 2, 5, 2, 3, 4, 2, 2, 3]
+    "name": "Michael Atkinson",
+    "photo": "https://images.unsplash.com/photo-1495538411606-c9efab92bcbe?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [5, 1, 3, 2, 1, 4, 5, 1, 4, 3]
+  },
+  {
+    "name": "Jerry Kennedy",
+    "photo": "https://images.unsplash.com/photo-1438763298591-75a0d42b7265?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 2, 2, 4, 1, 3, 5, 2, 1, 3]
+  },
+  { "name": "Jaeden Beard",
+    "photo": "https://images.unsplash.com/photo-1479685894911-37e888d38f0a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 1, 3, 4, 3, 4, 5, 5, 5, 3]
+  },
+  {
+    "name": "Craig Sheehan",
+    "photo": "https://images.unsplash.com/photo-1477458897488-99577e189ca6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 1, 1, 4, 3, 5, 5, 4, 5, 3]
+  },
+  {
+    "name": "Jerome Stephenson",
+    "photo": "https://images.unsplash.com/photo-1484517186945-df8151a1a871?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 1, 1, 4, 3, 5, 5, 4, 5, 3]
+  },
+  {
+    "name": "Andre Harris",
+    "photo": "https://images.unsplash.com/photo-1540760069740-f26c489ae0b2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 5, 4, 5, 3, 4, 5, 5, 5, 3]
+  },
+  {
+    "name": "Daryl White",
+    "photo": "https://images.unsplash.com/photo-1473492201326-7c01dd2e596b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 5, 4, 2, 3, 4, 2, 5, 5, 3]
+  },
+  {
+    "name": "Anita Herring",
+    "photo": "https://images.unsplash.com/photo-1528479539300-7f03264ca215?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 5, 4, 2, 3, 4, 2, 5, 5, 3]
+  },
+  {
+    "name": "Jewel Moore",
+    "photo": "https://images.unsplash.com/photo-1507763309844-52fce60a5481?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [1, 5, 2, 2, 3, 3, 2, 5, 5, 2]
+  },
+  {
+    "name": "Tracy Tyndall",
+    "photo": "https://images.unsplash.com/photo-1541717782351-e9acfdc4f0ab?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [5, 5, 1, 2, 1, 3, 1, 5, 5, 2]
+  },
+  {
+    "name": "Fiona Apple",
+    "photo": "https://images.unsplash.com/photo-1476722817135-8d33bf111a0c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 3, 1, 5, 5, 2]
+  },
+  {
+    "name": "Elsa Jarell",
+    "photo": "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {
+    "name": "Marcia Ellington",
+    "photo": "https://images.unsplash.com/photo-1541147602143-fb8d04ba9eca?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {
+    "name": "Abdul Kalami",
+    "photo": "https://images.unsplash.com/photo-1542463118-cdec6901db2a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {
+    "name": "Ankit Jain",
+    "photo": "https://images.unsplash.com/photo-1534339480783-6816b68be29c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9/",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {
+    "name": "Amit Bhadana",
+    "photo": "https://images.unsplash.com/photo-1542882038-6090d99603ac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {  
+    "name": "Chun-Li Ling",
+    "photo": "https://images.unsplash.com/photo-1496492299670-7615d3c37267?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [4, 5, 1, 4, 1, 2, 1, 3, 3, 2]
+  },
+  {
+    "name": "Chi Lin",
+    "photo": "https://images.unsplash.com/photo-1496499354870-877088652a58?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9",
+    "scores": [3, 3, 3, 4, 1, 2, 1, 3, 3, 2]
   }
-]
-
+  ]
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,16 +6,44 @@ var friends = require('../data/friends')
 
 module.exports =function (app){
     
-    app.get('/api/friends', function (req, res) {
+    app.get('/api/survey', function (req, res) {
         res.json(friends);
     })
 
-    app.post('/api/friends', function(req, res){
-        var addFriend = {
+    app.post('/api/survey', function(req, res){
+        var userInput = {
             name: req.body.name,
             photo: req.body.photo,
-            scores: []
+            scores: req.body.scores
         };
-    
+        console.log(req.body)
+
+        let userResponse=userInput.scores;
+
+        for (let i = 0; i < userInput.scores.length; i++){
+            userInput.scores[i]=parseInt(userResponse[i])
+        }
+
+        let matchName = ""
+        let matchImage =""
+        let totalDifference = 10000
+
+            for (let i = 0; i < friends.length; i++){
+                let eachDifference=0;
+                for (let j = 0; j < userResponse.length; j++){
+                    let score = userResponse[j];
+                    eachDifference += Math.abs(friends[i].scores[j]-score)
+                }
+                if (eachDifference < totalDifference){
+                    totalDifference=eachDifference;
+                    matchName=friends[i].name
+                    matchImage=friends[i].photo
+                }
+            }
+            friends.push(userInput)
+            res.json({
+                matchName: matchName,
+                matchImage: matchImage
+            })
     })
 }
